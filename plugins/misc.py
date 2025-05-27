236b62dd2793cd478a3e6cc221ddc27ad65e758c
tactition <zahidnazir330@gmail.com>
2025-05-27 11:25:24 +0530

    improved the ai check working code

--- plugins/misc.py
+++ plugins/misc.py
@@ -131,7 +131,7 @@ async def who_is(client, message):
         )
     await status_message.delete()
 
-@Client.on_message(filters.command(["library", "search"]))
+@Client.on_message(filters.command(["library", "bookinfo"]))
 async def imdb_search(client, message):
     if ' ' in message.text:
         k = await message.reply('Searching Books…')
